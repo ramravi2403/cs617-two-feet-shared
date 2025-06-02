63494f751fb68211b97cc259163bd487fabc69a0
Clyde Rapinan <rapinanc@amazon.com>
2025-06-01 23:34:57 -0700

    Add kl optimization for PPO

--- tuning/config/ppo_config.py
+++ tuning/config/ppo_config.py
@@ -11,20 +11,16 @@ PPO_CONFIG = OptimizationConfig(
     hyperparameters={
         # Learning rates
         'lr': ParameterRange(1e-5, 1e-3, log_scale=True),
-
         # Discount and GAE parameters
         'gamma': ParameterRange(0.9, 0.999),
-
         # PPO specific parameters
         'clip_ratio': ParameterRange(0.1, 0.3),
-
         # # Training parameters
-        # 'batch_size': ParameterRange(64, 256, step=64),
+        #x  'batch_size': ParameterRange(64, 256, step=64),
         # 'steps_per_epoch': ParameterRange(100, 1600, step=100),
         # 'train_iters': ParameterRange(20, 400, step=10),
-        #
         # # Normalization and clipping
-        # 'target_kl': ParameterRange(0.00, 0.03),
+        'target_kl': ParameterRange(0.00, 0.03),
         'n_steps': ParameterRange(8, 32, step=1),
     }
 )
